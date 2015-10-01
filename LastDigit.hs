81655b4905c70ad93debc29e04f185fccb9c85ef
Jon Wolski <jonwolski@gmail.com>
2015-09-30 20:34:35 -0500

    Attempt at using strict evaluation
    I still get out-of-memory errors for large inputs. It's probably lots of
    thunking. Attempting to use strict evaluation and TCO.

--- LastDigit.hs
+++ LastDigit.hs
@@ -1,12 +1,13 @@
 module LastDigit where
 
+import Data.List( foldl1' )
+
 lastDigit :: Integer -> Integer -> Integer
-lastDigit b e = iterate (partialProduct b ) 1 !! fromIntegral e
+lastDigit base exponent = foldl1' partialProduct [ unitsDigit base | _ <- [1..exponent]]
 
 partialProduct :: Integer -> Integer -> Integer
-partialProduct n = unitsDigit . (* n)
+partialProduct base = unitsDigit . (* base)
 
 unitsDigit :: Integer -> Integer
 unitsDigit = flip mod 10
-
 
