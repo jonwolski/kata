4824db04387cab7e4c3c1300d8ab46d641f31237
Jon Wolski <jonwolski@gmail.com>
2015-10-24 18:20:56 -0500

    append + prepend + insert

--- Balanced/Parens.hs
+++ Balanced/Parens.hs
@@ -1,19 +1,12 @@
 module Balanced.Parens ( balancedParens) where
 
-import Data.List
-import Data.Maybe
+import Data.List( nub )
 
 balancedParens :: Int -> [String]
-balancedParens n = filter balanced $ nub $ permutations $ take (2*n) $ cycle "()"
-
-balanced :: String -> Bool
-balanced (')':_) = False
-balanced s = 0 == (snd (fromMaybe ("",-1) (balance_depth (Just(s, 0)))))
-
-balance_depth :: Maybe (String, Integer) -> Maybe (String, Integer)
-balance_depth Nothing = Nothing
-balance_depth (Just (_, n)) | n < 0 = Nothing
-balance_depth (Just ('(':s, n)) = balance_depth $ Just (s, n+1)
-balance_depth (Just (')':s, n)) = balance_depth $ Just (s, n-1)
-balance_depth (Just ([], n)) = Just ([], n)
-
+balancedParens 0 = [""]
+balancedParens 1 = ["()"]
+balancedParens n = nub $ (append smallerList) ++ (prepend smallerList) ++ (insert smallerList )
+                   where smallerList = balancedParens (n -1)
+                         append  = map (\x -> x ++ "()")
+                         prepend = map (\x -> "()" ++ x)
+                         insert  =  map (\x -> ("(" ++ x ++ ")"))
