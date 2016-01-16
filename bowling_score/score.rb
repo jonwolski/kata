35ec930573c134a94e38b1fc6d915a45e825468e
Jon Wolski <jonwolski@gmail.com>
2016-01-16 15:02:19 -0600

    WIP

--- bowling_score/score.rb
+++ bowling_score/score.rb
@@ -1,12 +1,20 @@
 class Score
-  def calculate( frames )
-    get_eligible_frames( frames ).inject :+
+  def calculate( throws )
+    get_eligible_throws( throws ).inject :+
   end
 
   private
 
-  def get_eligible_frames( frames )
-    frames[ 0 .. frames.length / 2 * 2 - 1 ]
+  def get_eligible_throws( throws )
+    throws[ 0 .. throws.length / 2 * 2 - 1 ]
+  end
+
+
+  class State
+    def throw
+
+    end
   end
 
 end
+
