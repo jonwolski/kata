14a8ccd5e74584e058a46a3e79eff72bce4c7163
Jon Wolski <jonwolski@gmail.com>
2016-01-23 13:19:04 -0600

    Clean up types and code
    * User generic type for `Read`-able in `grid`.
    * Remove unnecessary lambda
    * Replace unused variable `x` with `_` in `xxs@(x:xs)`
    * Remove extra parens in pattern match of simply `(x)`

--- euler/11_largest_product_in_a_grid.hs
+++ euler/11_largest_product_in_a_grid.hs
@@ -22,8 +22,8 @@ grid_text = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 \n\
            \ 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 \n\
            \ 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
 
-grid :: [[Int]]
-grid  = map ((map (\x -> read x::Int)) . words ) $ lines grid_text
+grid :: (Read a) => [[a]]
+grid  = map ((map read) . words ) $ lines grid_text
 
 rows :: [[a]] -> [[a]]
 rows = id
@@ -35,8 +35,8 @@ diags :: [[a]] -> [[a]]
 diags = error "implement diags"
 
 productsOfQuartets :: (Num a) => [a] -> [a]
-productsOfQuartets xxs@(x:xs) = (product $ take 4 xxs):productsOfQuartets xs
-productsOfQuartets (x) = x
+productsOfQuartets xxs@(_:xs) = (product $ take 4 xxs):productsOfQuartets xs
+productsOfQuartets x = x
 
 maximumValue :: Int
 maximumValue = maximum $ concat $ map productsOfQuartets $ rows grid ++ cols grid
