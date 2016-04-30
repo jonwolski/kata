7a9eea02fad57a40e9e33fe5f65bd93b6683273c
Jon Wolski <jonwolski@gmail.com>
2016-04-30 08:18:01 -0500

    Print an Integer rather than a Maybe

--- euler/12_highly_divisible_triangular_number.hs
+++ euler/12_highly_divisible_triangular_number.hs
@@ -4,7 +4,10 @@ import Data.List (find)
 triangles :: [Integer]
 triangles = scanl1 (+) [1 .. ]
 
-numWithManyDivisors :: Maybe Integer
-numWithManyDivisors = find ((>= 500) . tau) triangles
+numWithManyDivisors :: Integer
+numWithManyDivisors = n
+  where (Just n) = find ((>= 500) . tau) triangles
+  -- no need to match `Nothing`. Since `triangles` is an infinite list, some value will meet the condition eventually.
+  -- Also, `find` will never return `Nothing`; it would fail to terminate if the condition were never met.
 
 main = putStrLn $ show $ numWithManyDivisors
